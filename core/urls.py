54228f3642a4e8a48d1ed8a123731c98b2829e02
cachululo <cachululop7@gmail.com>
2024-06-20 18:58:11 -0400

    se agrega vista registrar, se genera el registro con los datos necesarios para que se agregue a la base de datos de django, tambien se logea y lo deja en el inicio.

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, productos, exit
+from .views import home, productos, exit,register
 
 urlpatterns = [
     path('', home, name='home'),
     path('productos/', productos, name='productos'),
     path('logout/', exit, name='exit'),
+    path('register/', register, name='register'),
 ]
