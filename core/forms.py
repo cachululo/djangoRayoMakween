48fcebf61038daa0d986aa27e8d7ba5cbf7a71ef
cachululo <cachululop7@gmail.com>
2024-06-20 19:29:54 -0400

    se arregla login, faltan forms y carrito de compras

--- core/forms.py
+++ core/forms.py
@@ -8,3 +8,5 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model= User
         fields =['username','first_name','last_name','email','password1','password2']
+	
+    
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\fgom9\OneDrive\Desktop\proyecto makeen\djangoRayoMakween\venv
+set VIRTUAL_ENV=C:\Users\julia\Downloads\logeorayomkween\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
